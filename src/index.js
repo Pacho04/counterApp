1b2841ec7676cc81d18875253c1a8c54670200d5
ismael-creator <ismabarri41@gmail.com>
2020-08-05 18:37:11 -0300

    Seccion 4

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import PrimeraApp from './PrimeraApp';
+import './index.css';
+import CounterApp from './CounterApp';
+
+
+const divRoot = document.querySelector('#root');
+//le mandamos un dato al componente
+ReactDOM.render( /*<PrimeraApp saludo = "Hola, soy tu padre"/> */ <CounterApp value = {0} />, divRoot);
